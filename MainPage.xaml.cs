8d1c849962d6386bafb816b7fa838a296668b024
Di Kong <di.kong@navitaire.com>
2023-01-31 15:11:33 -0700

    initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace MauiApp3;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Pressed(object sender, EventArgs e)
+    {
+        Container.IsVisible = !Container.IsVisible;
+    }
+}
+
